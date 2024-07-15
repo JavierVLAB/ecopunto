d300a33498368e4db5736332b0af5bb2f73c7244
JaviVLAB <javier.zen@gmail.com>
2024-07-15 13:02:59 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI(docs_url="/api/doc")
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
+)
 
 @app.get("/api/python")
 def hello_world():
