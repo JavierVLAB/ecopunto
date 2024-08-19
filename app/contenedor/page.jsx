ff2f5e45718164abf167b54d5f4f80a6c5dba5c9
JaviVLAB <javier.zen@gmail.com>
2024-08-19 15:45:40 +0200

    update whatsapp

--- app/contenedor/page.jsx
+++ app/contenedor/page.jsx
@@ -132,11 +132,13 @@ export default function Contenedor() {
           
         </div>
 
-        <p className="font_caption text-grey05 mt-12 ms-6">¿OTRAS DUDAS?</p>
-        
-        <div className="px-7">
-          <Link href="/confirmacion" className="font_h3 text-ecovidrio_dark mt-1 underline">Escríbenos por Whatsapp</Link>
-          <p className="font_body_secundary text-grey05 mt-1">Horario de antención de 10-18hrs, <br/>de lunes a viernes</p>
+        <div className="fixed bottom-10">
+          <p className="font_caption text-grey05 mt-12 ms-6">¿OTRAS DUDAS?</p>
+          
+          <div className="px-7">
+            <Link href="/confirmacion" className="font_h3 text-ecovidrio_dark mt-1 underline">Escríbenos por Whatsapp</Link>
+            <p className="font_body_secundary text-grey05 mt-1">Horario de antención de 10-18hrs, <br/>de lunes a viernes</p>
+          </div>
         </div>
       </main>
 
